0b87bcf21ef19538182dd83ee76630f1ddb0fb06
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-12 11:50:02 +0000

    add & test code

---
+++ test_student.py
@@ -0,0 +1,79 @@
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from datetime import timedelta
+from student import Student
+
+
+class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("setupclass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("teardownclass")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("Lucas", "Behrendt")
+
+    def tearDown(self):
+        print("teardown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "Lucas Behrendt")
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "lucas.behrendt@email.com")
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(50)
+
+        self.assertEqual(self.student.end_date,
+                         old_end_date + timedelta(days=50))
+
+    # def test_apply_extension(self):
+    #     self.student.apply_extension(50)
+
+    #     self.assertEqual(self.student.end_date, self.student.end_date)
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request")
+
+    def test_get_start_date(self):
+        # self.student.get_start_date()
+
+        self.assertEqual(self.student._start_date, self.student.get_start_date())
+
+    @mock.patch.object(Student, "apply_extension")
+    def test_is_extended(self, mock):
+        self.student.apply_extension(5)
+        mock.assert_called()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
